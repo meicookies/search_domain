5b4a6e1549760c4c00cc3067dde76b466d182c62
meicookies <79348524+meicookies@users.noreply.github.com>
2021-10-05 17:28:11 +0700

    run ez

---
+++ search.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+try:
+    import requests, re, validators, os, time
+    from bs4 import BeautifulSoup
+    from alive_progress import alive_bar
+except ModuleNotFoundError:
+    os.system("pip install requests validators bs4 alive-progress")
+
+curl = requests.Session()
+curl.trust_env = False
+print("""
+
+    search domain by name
+
+""")
+keyword = input("[$] Search: ")
+
+sites = "https://crt.sh/"
+source = curl.get(f"{sites}?q={keyword}").content
+html = re.sub("<BR>", "\n", source.decode('UTF-8'))
+
+pocket = set()
+counter = 0
+print("[💕] Please wait...")
+for site in BeautifulSoup(html, "html.parser").find_all("td"):
+    if validators.domain(site.text):
+        with open(f"{keyword}.txt", "a+") as fuck:
+            if site.text not in pocket:
+                counter += 1
+                pocket.add(site.text)
+                fuck.write(re.sub("$", "\n", site.text))
+                fuck.close
+result = [counter]
+print(f"[❗] Found: {result} domain")
+for x in result:
+    with alive_bar(x) as bar:
+        for i in range(x):
+            time.sleep(.01)
+            bar()
+saved_result = re.sub("\s", "_", keyword)
+print(f"[✅] Tersimpan di {saved_result}.txt")
+# coded by ./meicookies
